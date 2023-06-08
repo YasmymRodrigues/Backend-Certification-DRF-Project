01e0f578822996287dc759ec6fe65675a81f7028
Yasmym Rodrigues <yasmymrdgs@gmail.com>
2023-06-08 10:21:00 -0300

    add values to the table Category and edited the views

--- RestaurantLLAPI/serializers.py
+++ RestaurantLLAPI/serializers.py
@@ -5,12 +5,13 @@ from rest_framework.decorators import api_view
 from .models import MenuItem
 from decimal import Decimal
 from .models import Category
+
 #import bleach
 
-@api_view()
-def menu_items(request):
-    items = MenuItem.objects.all()
-    return Response(items.values())
+# @api_view()
+# def menu_items(request):
+#     items = MenuItem.objects.all()
+#     return Response(items.values())
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -19,24 +20,27 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'slug', 'title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    title = serializers.CharField
-    price = serializers.DecimalField(max_digits=3, decimal_places=2)
+    #id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2)
     #stock = serializers.IntegerField(source = 'inventory')
     #price_after_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
-    featured = serializers.BooleanField
-    category = serializers.StringRelatedField(read_only=True)
+    featured = serializers.BooleanField()
+    category = CategorySerializer(read_only = True)
+    category_id = serializers.IntegerField(write_only = True)
+    #category = serializers.StringRelatedField(read_only=True)
     #category = CategorySerializer(read_only = True)
 
 
     class Meta:
             model = MenuItem
-            fields = ['id', 'title', 'price', 'featured' ,'category']
-            depth = 1
+            fields = ['id','title', 'price', 'featured', 'category', 'category_id']
+            #depth = 1
             extra_kwargs={
                 'price': {'min_value': 2},
                 #'stock': {'source':'inventory', 'min_value' : 0}
             }
 
 
-    def calculate_tax(self, product:MenuItem):
-        return product.price * Decimal(1.1)
+#     def calculate_tax(self, product:MenuItem):
+#         return product.price * Decimal(1.1)
--- RestaurantLLAPI/urls.py
+++ RestaurantLLAPI/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
-urlpatterns = [
-    #path('ratings', views.RatingsView.as_view()),
-    #path('books/', views.books),
-    #to map all the class as a view
-    #path('books', views.BookList.as_view()),
-    #path('books/<int:pk>', views.Book.as_view()),    
-    path('menu-items', views.MenuItemView.as_view()),  
+urlpatterns = [    
+    path('menu-items', views.MenuItemView.as_view()), 
+    path('menu-items/<int:id>', views.single_item), 
+    # path('menu-items', views.menu_items),
+    # path('menu-items/<int:id>', views.single_item),
 
 ]
